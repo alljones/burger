8e35bbb3762272a954517517819695b44e1296e9
Allan Jones <iamallanjones@gmail.com>
2018-05-07 12:29:16 -0500

    Added DB and seed data

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burgers_name, devoured)
+VALUES
+    ('Chicken Avocado Burger', false);
+INSERT INTO burgers
+    (burgers_name, devoured)
+VALUES
+    ('Chilli Double Cheese Burger', false);
+INSERT INTO burgers
+    (burgers_name, devoured)
+VALUES
+    ('Santa Fe Spicy Burger', false);
