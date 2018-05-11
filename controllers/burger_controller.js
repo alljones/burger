098dbdd8617dc465bafe49bb212d1b0893e100b2
Allan Jones <iamallanjones@gmail.com>
2018-05-10 23:42:25 -0500

    Update functionality of each route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -7,48 +7,36 @@ var router = express.Router();
 
 // = = = = = = = = = = = = = = = = = = = = = = =
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+// Create the routes and associated logic
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.insertOne(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    }
-  );
+router.post("/burgers", function(req, res) {
+  burger.insertOne(["burger_name"], [req.body.burger_name], function(data) {
+    res.redirect("/");
+  });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
-  cat.update(
+  burger.updateOne(
     {
-      sleepy: req.body.devoured
+      devoured: true
     },
     condition,
-    function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+    function(data) {
+      res.redirect("/");
     }
   );
 });
